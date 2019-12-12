26b9029ecb09f3efaf71439f72f0d2e6e74f65f1
Michael Ashton <michael.ashton@caktusgroup.com>
2019-12-12 13:05:12 -0500

    removes some old stuff, including a couple unneeded dependencies

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 // Styles
 import styled, { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/globalStyles";
 import defaultTheme from "./styles/defaultTheme";
 
-// React Router
-import { Route } from "react-router-dom";
 
-// Children
-import SideBar from "./components/SideBar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import CollectionMessages from "./components/Collection/CollectionMessages";
-import AppContext from "./components/app-state";
-import MessageDetail from "./components/Message/MessageDetail";
 
 const THEME = defaultTheme;
 
@@ -27,52 +18,19 @@ const AppStyled = styled.div`
   color: ${THEME.primaryTextWhite};
 `;
 
-const MainContent = styled.main`
-  border: 1px solid green;
-  width: 100%;
-`;
+// const MainContent = styled.main`
+//   border: 1px solid green;
+//   width: 100%;
+// `;
 
 function App() {
-  const [collections, setCollections] = useState([]);
-  const [currentCollection, setCurrentCollection] = useState();
-  const [messages, setMessages] = useState([]);
-  const [currentMessageId, setCurrentMessageId] = useState();
-
   return (
     <ThemeProvider theme={THEME}>
       <GlobalStyle />
       <AppStyled>
-        <AppContext.Provider
-          value={{
-            collections,
-            setCollections,
-            currentCollection,
-            setCurrentCollection,
-            messages,
-            setMessages,
-            currentMessageId,
-            setCurrentMessageId
-          }}
-        >
-          <SideBar />
-          <MainContent>
-            <Route exact path="/">
-              <Home />
-            </Route>
 
-            <Route path="/about">
-              <About />
-            </Route>
+        <h1>RATOM tho</h1>
 
-            <Route exact path="/collection/:collectionId">
-              <CollectionMessages />
-            </Route>
-
-            <Route path="/collection/:collectionId/message/:messageId">
-              <MessageDetail />
-            </Route>
-          </MainContent>
-        </AppContext.Provider>
       </AppStyled>
     </ThemeProvider>
   );
