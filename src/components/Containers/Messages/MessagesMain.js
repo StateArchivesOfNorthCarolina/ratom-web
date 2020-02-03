d8f61c4ed1e82b87809e109b6d984bbd9b54c23a
Michael Ashton <michael.ashton@caktusgroup.com>
2020-02-03 11:44:43 -0500

    merge conflict resolution messed up one little hting

--- src/components/Containers/Messages/MessagesMain.js
+++ src/components/Containers/Messages/MessagesMain.js
@@ -40,6 +40,12 @@ const MessagesMain = () => {
   const { path } = useRouteMatch();
 
   useEffect(() => {
+    Axios.get(`${SHOW_ACCOUNT}${accountId}/`).then(response => {
+      setAccount(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
     searchMessages();
   }, [query]);
 
