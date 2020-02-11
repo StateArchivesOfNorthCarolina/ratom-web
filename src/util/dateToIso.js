e9cd200d15f9eb3744836ee14b1c26faeb029c32
Michael Ashton <michael.ashton@caktusgroup.com>
2020-02-11 09:40:52 -0500

    fixes it

--- src/util/dateToIso.js
+++ src/util/dateToIso.js
@@ -1,6 +1,7 @@
 import { format } from 'date-fns';
 
 const dateToIso = date => {
+  if (!date) return '';
   // Safari sucks at dates, evidently. If date doesn't contain literal T between date and time (ISO8601), add it...
   const safeDate = date.replace(' ', 'T');
   return format(new Date(safeDate), 'yyyy-MM-dd');
