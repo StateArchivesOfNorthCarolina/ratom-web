37e623a3af232e32a2f5c4a26e27777d60e5ac52
Michael Ashton <michael.ashton@caktusgroup.com>
2020-02-27 11:20:05 -0500

    finish resolution of conflict

--- src/util/filterConstructors/dateRangeFilterBuilderAND.js
+++
@@ -1,6 +0,0 @@
-const dateRangeFilterBuilderAND = dateRange => {
-  const [fromDate, toDate] = dateRange;
-  return `sent_date__range=${fromDate}__${toDate}`;
-};
-
-export default dateRangeFilterBuilderAND;
