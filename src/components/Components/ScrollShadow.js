ef7966a0f44d17f67c23850d44624d333673a30a
Michael Ashton <michael.ashton@caktusgroup.com>
2020-03-02 14:04:25 -0500

    remove commented code

--- src/components/Components/ScrollShadow.js
+++ src/components/Components/ScrollShadow.js
@@ -29,40 +29,6 @@ const InnerShadow = styled.div`
     `inset 0 ${props.position === 'bottom' ? '-' : ''}45px 35px -30px ${colorWhite}`};
 `;
 
-// const ShadowWrapperTop = styled.div`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   height: 8rem;
-//   width: 100%;
-// `;
-
-// const InnerShadowTop = styled.div`
-//   position: fixed;
-//   z-index: 999;
-//   height: 8rem;
-//   width: props
-//   box-shadow: inset 0 45px 35px -30px ${colorWhite};
-// `;
-
-// const ShadowWrapperBottom = styled.div`
-//   position: absolute;
-//   bottom: 0;
-//   left: 0;
-//   right: 0;
-//   height: 8rem;
-//   width: props;
-// `;
-
-// const InnerShadowBottom = styled.div`
-//   position: fixed;
-//   z-index: 999;
-//   height: 8rem;
-//   width: 26rem;
-//   box-shadow: inset 0 -45px 35px -30px ${colorWhite};
-// `;
-
 ScrollShadow.propTypes = {
   position: PropTypes.oneOf(['top', 'bottom']).isRequired,
   innerWidth: PropTypes.string.isRequired
