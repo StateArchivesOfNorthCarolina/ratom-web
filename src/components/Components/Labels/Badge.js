c065ad0023758fcad837e342ae14ae2e8bbdcf55
Michael Ashton <michael.ashton@caktusgroup.com>
2020-03-06 14:24:06 -0500

    replace missing import

--- src/components/Components/Labels/Badge.js
+++ src/components/Components/Labels/Badge.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { colorBadgeBlue, colorBadgeGreen } from '../../../styles/styleVariables';
+import { colorBadgeBlue, colorBadgeGreen, colorBadgeRed } from '../../../styles/styleVariables';
 
 import { darken, lighten } from '../../../styles/styleUtils/lighten-darken';
 
